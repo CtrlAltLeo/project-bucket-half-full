0f95fc413c9402ed0a2677fe66991604cf9a11e2
AquinasSoftware <aidan@conantguy.org>
2026-02-23 17:08:17 -0600

    Added Feature Detection POC

--- OpenCV/Masking/computeMask.py
+++ OpenCV/Masking/computeMask.py
@@ -2,11 +2,25 @@ import cv2 as cv
 import numpy as np
 import os
 
+#Import Photos
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+img_path = os.path.join(script_dir, "angle1.jpg")
+img1 = cv.imread(img_path)
+img1 = cv.resize(img1, (960,720), cv.INTER_AREA)
+
+img_path = os.path.join(script_dir, "angle2.jpg")
+img2 = cv.imread(img_path)
+img2 = cv.resize(img2, (960,720), cv.INTER_AREA)
+
 # Configurables
 
-F = 750
-Cx = 320
-Cy = 240
+height, width = img1.shape[:2]
+
+F = 0.8 * width
+Cx = width / 2
+Cy = height / 2
 
 K = np.array([
     [F, 0, Cx],
@@ -14,30 +28,27 @@ K = np.array([
     [0, 0, 1]
 ], dtype=np.float64)
 
-#Import Photos
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-img_path = os.path.join(script_dir, "boxCam1.png")
-img1 = cv.imread(img_path)
-
-img_path = os.path.join(script_dir, "boxCam2.png")
-img2 = cv.imread(img_path)
-
 # Feature Detection
 
-orb = cv.ORB_create(nfeatures = 500)
+orb = cv.ORB_create(nfeatures=5000)
 
 keyPoints1, descriptor1 = orb.detectAndCompute(img1, None)
 keyPoints2, descriptor2 = orb.detectAndCompute(img2, None)
 
 bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
 matches = bf.match(descriptor1, descriptor2)
+
+# Sort matches by quality (optional but recommended)
 matches = sorted(matches, key=lambda x: x.distance)
+
+pts1 = np.float64([keyPoints1[m.queryIdx].pt for m in matches])
+pts2 = np.float64([keyPoints2[m.trainIdx].pt for m in matches])
 
 cam1Pts = np.float64([keyPoints1[m.queryIdx].pt for m in matches])
 cam2Pts = np.float64([keyPoints2[m.trainIdx].pt for m in matches])
 
+
+print(len(matches))
 # Calculate Essential MAtrix
 
 E, mask = cv.findEssentialMat(cam1Pts, cam2Pts, K, 
@@ -45,13 +56,15 @@ E, mask = cv.findEssentialMat(cam1Pts, cam2Pts, K,
 
 inliers, R, t, mask = cv.recoverPose(E, cam1Pts, cam2Pts, K)
 
+print(len(mask))
+
 # Remove Outlier Points
 mask = mask.ravel().astype(bool)
 
 cam1Pts_in = cam1Pts[mask]
 cam2Pts_in = cam2Pts[mask]
 
-# Build Projection MAtrixes
+# Build Projection Matrixes
 
 cam1 = np.hstack((np.eye(3), np.zeros((3,1))))
 cam2 = np.hstack((R, t))
@@ -73,15 +86,12 @@ triangulatedPts = triangulatedPts.T
 
 Z = triangulatedPts[:,2]
 
-box = Z < np.median(Z)
+book = Z < np.mean(Z)
 
-projected, _ = cv.projectPoints(triangulatedPts, np.zeros(3), np.zeros(3), K, None)
-projected = projected.reshape(-1, 2).astype(int)
+# Draw Mask
+for pt in cam1Pts_in:
+    cv.circle(img1, tuple(np.int32(pt)), 3, (0,255,0), -1)
 
-# Draw MAsk
+cv.imshow("Masked Image", img1)
 
-for i, pt in enumerate(projected):
-    if box[i]:
-        cv.circle(img1, tuple(pt), 3, (0,255,0), -1)
-
-cv.imshow("img1", img1)
+cv.waitKey(0)
